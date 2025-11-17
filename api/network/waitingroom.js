7b395624d4db1a2d63e7426ff8f698f75dd1ca25
Aorux01 <139909408+Aorux01@users.noreply.github.com>
2025-11-17 20:29:38 +0100

    Delete api directory

--- api/network/waitingroom.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/waitingroom/api/waitingroom', (req, res) => {
-    res.status(204).send();
-});
-
-module.exports = router;
