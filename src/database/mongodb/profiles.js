c728ea380b6d770bf0ffab2c2d3914c7d4c684cc
Aorux01 <139909408+Aorux01@users.noreply.github.com>
2026-02-03 19:05:03 +0100

    Delete src directory

--- src/database/mongodb/profiles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProfileSchema = new mongoose.Schema({
-    accountId: { type: String, required: true, unique: true },
-    created: { type: Date, required: true },
-    profiles: { type: Object, required: true }
-}, { collection: 'profiles' });
-
-module.exports = mongoose.model('Profile', ProfileSchema);
