385ded0f51d0a5c8386caa87ed2b02a96c61d29d
Aorux01 <139909408+Aorux01@users.noreply.github.com>
2026-02-11 13:35:27 +0100

    Delete api directory

--- api/datarouter/datarouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post('/datarouter/api/v1/public/data', async (req, res) => {
-    res.status(204).end();
-});
-
-module.exports = router;
